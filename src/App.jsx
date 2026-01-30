ce6f548788b2b13b3df1566fa42a99ad73221f4e
Jerrisneh <jerrisneh@gmail.com>
2026-01-30 19:32:57 +0100

    updated the three components without css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import Counter from "./Counter";
+import TodoList from "./TodoList";
+import UserData from "./userData";
 
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <Counter />
+      <TodoList />
+      <UserData />
+    </>
+  );
 }
